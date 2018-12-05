fec28d5a634161000311a12bf4972e5672228f5f
Veljko Dimitrijevic <reduvius@protonmail.com>
2018-12-05 12:39:23 +0100

    Display welcome message when a user logs in

--- src/Controllers/WelcomeController.php
+++ src/Controllers/WelcomeController.php
@@ -2,12 +2,25 @@
 
 namespace MyBlog\Controllers;
 
+use MyBlog\Models\UserModel;
+
 class WelcomeController extends AbstractController {
 	// Get welcome page
 	public function getWelcomePage(): string {
-		$properties = [
-			'isAuth' => $this->isAuthenticated()
-		];
+		if ($this->returnUserCookie()) {
+		    $userModel = new UserModel($this->db);
+            $user = $userModel->getUserById($this->returnUserCookie());
+
+		    $properties = [
+			    'user' => $user,
+				'uId' => $this->returnUserCookie(),
+			    'isAuth' => $this->isAuthenticated()
+		    ];
+		} else {
+			$properties = [
+			    'isAuth' => $this->isAuthenticated()
+		    ];
+		}
 		return $this->render('welcome.twig', $properties);
 	}
 }
